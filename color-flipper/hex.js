b0cf59835bff8e2459fa3e4886c881a6fb1d85fb
Gabriel Estevam <gabriel.estevam2204@gmail.com>
2023-07-01 21:56:02 -0300

    Color Flipper

---
+++ color-flipper/hex.js
@@ -0,0 +1,39 @@
+const hex = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  let hexColor = "#";
+  for (i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*hex.length)
+}
