cbbc8624b8128277d0ec51815d283b1deddb9021
Gabriel Estevam <gabriel.estevam2204@gmail.com>
2023-07-01 22:45:57 -0300

    .

--- counter/app.js
+++ counter/app.js
@@ -1,6 +1,34 @@
 let count = 0;
 
 const value = document.querySelector("#value");
-const btns = document.querySelector(".btn");
+const decreaseBtn = document.querySelector(".decrease");
+const resetBtn = document.querySelector(".reset");
+const increaseBtn = document.querySelector(".increase");
 
+decreaseBtn.addEventListener("click", () => {
+  count--;
+  value.textContent = count;
+  if (count < 0) {
+    value.style.color = "red";
+  } else if (count == 0) {
+    value.style.color = "black";
+  }
+});
 
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  value.textContent = count;
+  if (count == 0) {
+    value.style.color = "black";
+  }
+});
+
+increaseBtn.addEventListener("click", () => {
+  count++;
+  value.textContent = count;
+  if (count > 0) {
+    value.style.color = "green";
+  } else if (count == 0) {
+    value.style.color = "black";
+  }
+});
